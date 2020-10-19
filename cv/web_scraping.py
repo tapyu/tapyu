aed2e1e9babd074742618e0b66668c1ed8c0ed14
Tapyu <rubem.engenharia@gmail.com>
2020-10-18 23:04:10 -0300

    cv commit

--- cv/web_scraping.py
+++ cv/web_scraping.py
@@ -5,4 +5,3 @@ import pandas as pd
 driver = webdriver.Chrome("C:\\Users\\rubem\\Downloads\\chromedriver_win32\\chromedriver.exe")
 
 driver.get('file:///C:/Users/rubem/Documents/GitHub/Tapyu/cv/curriculo.html')
-print('anything')soup.select('#author')
